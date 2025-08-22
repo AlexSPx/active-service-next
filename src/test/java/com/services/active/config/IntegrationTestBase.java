fc7ad9d3aca31b4495c3c9a91dd85c8b9255b0d5
alexspx <alexspx@hotmail.com>
2025-08-22 22:45:26 +0300

    IntegrationTestBase

---
+++ src/test/java/com/services/active/config/IntegrationTestBase.java
@@ -0,0 +1,35 @@
+package com.services.active.config;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+public abstract class IntegrationTestBase {
+
+    @Container
+    private static final MongoDBContainer MONGO = new MongoDBContainer("mongo:7.0.14");
+
+    @BeforeAll
+    static void startContainer() {
+        MONGO.start();
+    }
+
+    @AfterAll
+    static void stopContainer() {
+        MONGO.stop();
+    }
+
+    @DynamicPropertySource
+    static void mongoProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", MONGO::getReplicaSetUrl);
+        registry.add("jwt.secret", () -> "really_long_secret_that_is_at_least_256_bits_long");
+    }
+}
+
