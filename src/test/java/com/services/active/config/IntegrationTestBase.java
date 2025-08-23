9c764981d38440860f06f0fcacb5abfa687e957f
alexspx <alexspx@hotmail.com>
2025-08-23 12:09:41 +0300

    cleanup db after each test

--- src/test/java/com/services/active/config/IntegrationTestBase.java
+++ src/test/java/com/services/active/config/IntegrationTestBase.java
@@ -17,6 +17,8 @@ public abstract class IntegrationTestBase {
     @Container
     private static final MongoDBContainer MONGO = new MongoDBContainer("mongo:7.0.14");
 
+    private static final String TEST_DB = "active_test";
+
     @BeforeAll
     static void startContainer() {
         MONGO.start();
@@ -29,12 +31,21 @@ public abstract class IntegrationTestBase {
 
     @AfterEach
     void cleanUpDatabase() throws IOException, InterruptedException {
-        MONGO.execInContainer("mongosh", "--eval", "db.getSiblingDB('test').dropDatabase()");
+        MONGO.execInContainer("mongosh", "--eval", "db.getSiblingDB('" + TEST_DB + "').dropDatabase()");
     }
 
     @DynamicPropertySource
     static void mongoProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.data.mongodb.uri", MONGO::getReplicaSetUrl);
+        // Ensure the Spring app uses the same DB name as cleanup
+        registry.add("spring.data.mongodb.uri", () -> {
+            String rsUrl = MONGO.getReplicaSetUrl();
+            // Replace default '/test' database in URL with '/active_test'
+            String updated = rsUrl.replace("/test?", "/" + TEST_DB + "?");
+            if (updated.equals(rsUrl)) {
+                updated = rsUrl.endsWith("/test") ? rsUrl.substring(0, rsUrl.length() - 4) + TEST_DB : rsUrl;
+            }
+            return updated;
+        });
         registry.add("jwt.secret", () -> "really_long_secret_that_is_at_least_256_bits_long");
     }
 }
